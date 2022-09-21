877a1b11c86e71b604e66bf033fa895786b9bc88
Huukssu <113489686+Huukssu@users.noreply.github.com>
2022-09-21 10:06:38 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,3 +1,56 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(min)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    if (currenttemperature > 20) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    }
+    if (currenttemperature <= 20 && currenttemperature > 10) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # # # # #
+            . . . . .
+            `)
+    }
+    if (currenttemperature <= 10) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            # # # . #
+            # . # # #
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(max)
+    basic.clearScreen()
+})
+let min = 0
+let max = 0
+let currenttemperature = 0
+currenttemperature = input.temperature()
+max = currenttemperature
+min = currenttemperature
 basic.forever(function () {
-	
+    basic.showString(".")
+    basic.showNumber(input.temperature())
+    if (currenttemperature < min) {
+        min = currenttemperature
+    }
+    if (currenttemperature > max) {
+        max = currenttemperature
+    }
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.pause(1000)
 })
